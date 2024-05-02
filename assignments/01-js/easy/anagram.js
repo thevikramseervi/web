cdf8214396e6247ef80ad9dce598bd2b51df20ab
vikramseervi <vikramseervi2004@gmail.com>
2024-05-02 10:52:05 +0530

    anagram.js

--- assignments/01-js/easy/anagram.js
+++ assignments/01-js/easy/anagram.js
@@ -5,7 +5,26 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.length != str2.length) {
+    return false;
+  }
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  const len = str1.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (str1[i] != str2[j]) {
+        if (j == len - 1) {
+          return false;
+        }
+        continue;
+      }
+      else {
+        break;
+      }
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
